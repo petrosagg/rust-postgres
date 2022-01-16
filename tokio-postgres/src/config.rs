ac41ab906f13cd93c0549a932e5d171bcb20925c
Nikhil Benesch <nikhil.benesch@gmail.com>
2022-01-16 02:37:22 -0500

    Fix conditional imports

--- tokio-postgres/src/config.rs
+++ tokio-postgres/src/config.rs
@@ -19,7 +19,8 @@ use std::ops::Deref;
 #[cfg(unix)]
 use std::os::unix::ffi::OsStrExt;
 #[cfg(unix)]
-use std::path::{Path, PathBuf};
+use std::path::Path;
+use std::path::PathBuf;
 use std::str;
 use std::str::FromStr;
 use std::time::Duration;
